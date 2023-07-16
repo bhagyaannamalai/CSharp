22f403fca2a04ec45b7976f68a20cc14b072aa80
Bhagyalakshmi Annamalai <bhagya.alux@gmail.com>
2023-07-16 18:12:29 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public delegate int calculation(int x,int y);
+//multicast delegate
+public delegate void myDelegate();
+public class Program
+{
+    static void Main(string[] args)
+    {
+maths obj = new maths();
+calculation calc;
+calc=obj.Add;
+int result=calc(2,3);
+Console.WriteLine("Add: " + result);
+calc = obj.subtract;
+result=calc(4,2);
+Console.WriteLine("Subtract: " + result);
+
+myDelegate multidel = obj.method1;
+multidel+=obj.method2;
+multidel();
+
+
+    }
+}
+public class maths
+{
+    public int Add(int x, int y)
+    {
+        return x+y;
+    }
+    public int subtract(int x,int y)
+    {
+        return x-y;
+    }
+    public void method1()
+    {
+        Console.WriteLine("I am Method 1");
+    }
+    public void method2()
+    {
+        Console.WriteLine("I am Method 2");
+    }
+}
